b94ac5c366bb019a02261ab09a35c18b0c5bfb57
Mohammed alamoudi <134002587+Mohammed-Invs7@users.noreply.github.com>
2025-03-25 04:35:26 +0300

    Update settings.py

--- BackEnd/AI_Radiologist/AI_Radiologist/settings.py
+++ BackEnd/AI_Radiologist/AI_Radiologist/settings.py
@@ -194,7 +194,7 @@ ROOT_URLCONF = 'AI_Radiologist.urls'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_USE_TLS = False
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "asdmloon1@gmail.com" # email sending address
 EMAIL_HOST_PASSWORD = "kdyv rwro yxxw pzqa"
 
