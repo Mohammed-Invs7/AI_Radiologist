c46d5d45a71011d8911e67d8f9a11b100eef7341
Mohammed alamoudi <134002587+Mohammed-Invs7@users.noreply.github.com>
2025-04-27 19:18:48 +0300

    backend: Ignore models folder
    Ignore models folder to prevents git to push big models files

--- BackEnd/AI_Radiologist/media/models/1_our-Xray/get_report.py
+++
@@ -1,2 +0,0 @@
-def get_report(image_path, model_folder_path):
-    return "The cardiac silhouette and mediastinum size are within normal limits. There is no pulmonary edema. There is no focal consolidation. There are no XXXX of a pleural effusion. There is no evidence of pneumothorax."
